25ebe124ae3cc99db5842826cdf9306a0feb863f
Muhammad Fawwaz <muhammad.fawwaz22@ui.ac.id>
2026-06-14 19:49:35 +0700

    fix: delete dump

--- scratch/drop_weight.js
+++
@@ -1,7 +0,0 @@
-const { pool } = require('../src/config/database.js');
-async function main() {
-  await pool.query('ALTER TABLE sub_action_plans DROP COLUMN IF EXISTS weight;');
-  console.log('Column weight dropped from sub_action_plans.');
-  process.exit(0);
-}
-main().catch(console.error);
